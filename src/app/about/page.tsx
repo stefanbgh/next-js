39cc07c77f748f2f07069820a8057e5e555b6ff6
stefanbgh <stefan022github@gmail.com>
2025-03-23 15:36:06 +0100

    routing

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function About() {
+	return (
+		<section>
+			<h2>About page</h2>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea est
+				praesentium maxime.
+			</p>
+		</section>
+	);
+}
