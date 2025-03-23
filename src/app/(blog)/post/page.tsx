338ffe1232fc5affc832a63a43f1f77a12ebe509
stefanbgh <stefan022github@gmail.com>
2025-03-23 17:44:36 +0100

    linking navigation

--- src/app/(blog)/post/page.tsx
+++ src/app/(blog)/post/page.tsx
@@ -1,11 +1,24 @@
+import Link from "next/link";
+
 export default function Post() {
 	return (
 		<section>
 			<h2>Post</h2>
 			<ul>
-				<li>Post 1</li>
-				<li>Post 2</li>
-				<li>Post 3</li>
+				<li className="border py-4 px-6 w-fit mb-2">
+					<p>Post 1</p>
+					<Link href="/post/1">View more</Link>
+				</li>
+				<li className="border py-4 px-6 w-fit mb-2">
+					<p>Post 2</p>
+					<Link href="/post/2">View more</Link>
+				</li>
+				<li className="border py-4 px-6 w-fit">
+					<p>Post 3</p>
+					<Link href="/post/3" replace>
+						View more
+					</Link>
+				</li>
 			</ul>
 		</section>
 	);
