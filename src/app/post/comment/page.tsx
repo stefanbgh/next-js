75450d8bf3339111c79c687f7b229eec7224f6e9
stefanbgh <stefan022github@gmail.com>
2025-03-23 15:40:05 +0100

    nested routes

---
+++ src/app/post/comment/page.tsx
@@ -0,0 +1,12 @@
+export default function Comment() {
+	return (
+		<section>
+			<h2>Comment</h2>
+			<ul>
+				<li>Comment 1</li>
+				<li>Comment 2</li>
+				<li>Comment 3</li>
+			</ul>
+		</section>
+	);
+}
---
+++ src/app/post/page.tsx
@@ -0,0 +1,12 @@
+export default function Post() {
+	return (
+		<section>
+			<h2>Post</h2>
+			<ul>
+				<li>Post 1</li>
+				<li>Post 2</li>
+				<li>Post 3</li>
+			</ul>
+		</section>
+	);
+}
